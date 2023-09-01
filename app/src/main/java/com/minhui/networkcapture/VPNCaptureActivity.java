68cd75f40fd4a118d21200e7de075656ab9fc3e7
DeFexCode <143798005+DeFexCode@users.noreply.github.com>
2023-09-01 13:02:11 +0300

    Add files via upload

--- app/src/main/java/com/minhui/networkcapture/VPNCaptureActivity.java
+++ app/src/main/java/com/minhui/networkcapture/VPNCaptureActivity.java
@@ -1,19 +1,3 @@
-/*
-** Copyright 2015, Mohamed Naufal
-**
-** Licensed under the Apache License, Version 2.0 (the "License");
-** you may not use this file except in compliance with the License.
-** You may obtain a copy of the License at
-**
-**     http://www.apache.org/licenses/LICENSE-2.0
-**
-** Unless required by applicable law or agreed to in writing, software
-** distributed under the License is distributed on an "AS IS" BASIS,
-** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-** See the License for the specific language governing permissions and
-** limitations under the License.
-*/
-
 package com.minhui.networkcapture;
 
 import android.Manifest;
@@ -91,22 +75,16 @@ public class VPNCaptureActivity extends FragmentActivity {
     public static Context mContext;
 
     ProxyConfig.VpnStatusListener vpnStatusListener = new ProxyConfig.VpnStatusListener() {
-
         @Override
         public void onVpnStart(Context context) {
-            handler.post(()->vpnButton.setImageResource(R.mipmap.ic_stop));
-
-
-
+            handler.post(() -> vpnButton.setImageResource(R.mipmap.ic_stop));
             startRadarWindowService();
-
         }
 
         @Override
         public void onVpnEnd(Context context) {
-            handler.post(()->vpnButton.setImageResource(R.mipmap.ic_start));
-            if(windowService!=null)
-            {
+            handler.post(() -> vpnButton.setImageResource(R.mipmap.ic_start));
+            if (windowService != null) {
                 stopService(windowService);
             }
         }
@@ -126,33 +104,27 @@ public class VPNCaptureActivity extends FragmentActivity {
 
     private Handler handler;
 
-
-
     MainFragment mainFragment;
 
     @Override
     public void onBackPressed() {
-
         finishAffinity(); // Close all activities
         stopService(windowService);
         System.exit(0);
     }
 
     boolean execution = true;
-
     TextView days;
-
 
     ActivityResultLauncher<Intent> overlayPermissionResultLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             result -> {
                 if (!Settings.canDrawOverlays(this)) {
                     // You don't have the permission
-                    // Handle the case where user declined the permission
+                    // Handle the case where the user declined the permission
                 }
             }
     );
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -161,154 +133,74 @@ public class VPNCaptureActivity extends FragmentActivity {
 
         mContext = this;
 
-
-
         String value = getIntent().getStringExtra("logged");
-        float daysAmount = getIntent().getFloatExtra("days",0);
+        float daysAmount = getIntent().getFloatExtra("days", 0);
         days = findViewById(R.id.days);
 
-
-
-
-        if(!value.equals("true"))
-        {
-
-            Intent intent = new Intent(VPNCaptureActivity.this, LoginActivity.class);
-            startActivity(intent);
+        if (!value.equals("true") || daysAmount <= 0) {
+            startLoginActivity(); // Launch the login activity if login fails or days are depleted
         }
-        if(daysAmount<=0)
-        {
-
-            Intent intent = new Intent(VPNCaptureActivity.this, LoginActivity.class);
-            startActivity(intent);
-        }
-
-
-
-
 
         DecimalFormat decimalFormat = new DecimalFormat("#.0");
         String formattedNumber = decimalFormat.format(daysAmount);
-
-        days.setText("Days left :" + formattedNumber);
-
+        days.setText("Days left: " + formattedNumber);
 
         String login = getIntent().getStringExtra("login");
         String password = getIntent().getStringExtra("password");
-        Thread t1   = new Thread(() ->{
 
-        while (execution)
-        {
+        Thread t1 = new Thread(() -> {
+            while (execution) {
+                String apiKey = "7psdgdsg3241124123tgcxvczSFD124412D5adfJEBACCHINY44f3265taaFDdgs36ft3546t346XDD5t4128n0HWuTbxz777vfddsgMkDXgdsw3gdsg214erqw";
 
+                LoginDataJSON loginDataJSON = new LoginDataJSON();
+                loginDataJSON.name = login;
+                loginDataJSON.password = password;
+                String loginJSON = gson.toJson(loginDataJSON);
 
+                HttpTask.Callback callback = new HttpTask.Callback() {
+                    @RequiresApi(api = Build.VERSION_CODES.O)
+                    @Override
+                    public void onSuccess(String response) {
+                        // Handle success
+                    }
 
+                    @Override
+                    public void onFailure(String errorMessage) {
+                        runOnUiThread(() -> {
+                            startLoginActivity(); // Launch the login activity on failure
+                        });
+                        execution = false;
+                    }
+                };
 
-            String apiKey = "7psdgdsg3241124123tgcxvczSFD124412D5adfJEBACCHINY44f3265taaFDdgs36ft3546t346XDD5t4128n0HWuTbxz777vfddsgMkDXgdsw3gdsg214erqw";
+                HttpTask postTask = new HttpTask(apiKey, callback);
+                postTask.sendPostRequest(Api.updateUserLoginDate, loginJSON, "PUT");
 
-            LoginDataJSON loginDataJSON = new LoginDataJSON();
-
-            loginDataJSON.name  = login;
-            loginDataJSON.password = password;
-            String loginJSON = gson.toJson(loginDataJSON);
-
-            HttpTask.Callback callback = new HttpTask.Callback() {
-                @RequiresApi(api = Build.VERSION_CODES.O)
-                @Override
-                public void onSuccess(String response) {
-
-
+                try {
+                    Thread.sleep(20000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-
-
-
-                @Override
-                public void onFailure(String errorMessage) {
-
-
-
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-
-                            Intent intent = new Intent(VPNCaptureActivity.this, LoginActivity.class);
-                            startActivity(intent);
-
-                        }
-                    });
-                    execution = false;
-
-                }
-
-
-            };
-
-
-
-
-            HttpTask postTask = new HttpTask(apiKey, callback);
-
-
-            postTask.sendPostRequest(Api.updateUserLoginDate, loginJSON , "PUT");
-
-
-
-            try {
-                Thread.sleep( 20000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
             }
-        }
-
-
-
-
-
-
-
-
         });
 
         t1.start();
-
-
-
-
-
-
-
-
-
         getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.toolbar));
-        vpnButton = (ImageView) findViewById(R.id.vpn);
-        vpnButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-              if(VpnServiceHelper.vpnRunningStatus()){
-                  closeVpn();
-              }else {
-                  startVPN();
-              }
+        vpnButton = findViewById(R.id.vpn);
+        vpnButton.setOnClickListener(v -> {
+            if (VpnServiceHelper.vpnRunningStatus()) {
+                closeVpn();
+            } else {
+                startVPN();
             }
         });
 
-
-
-
-
-   //     packageId = (TextView) findViewById(R.id.package_id);
-        initMainFragment();
-
-
         requestStoragePermission();
-
-
-
         vpnButton.setEnabled(true);
         ProxyConfig.Instance.registerVpnStatusListener(vpnStatusListener);
 
         if (!Settings.canDrawOverlays(this)) {
-            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse("package:" + getPackageName()));
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
             overlayPermissionResultLauncher.launch(intent);
         }
 
@@ -319,37 +211,19 @@ public class VPNCaptureActivity extends FragmentActivity {
                 .addToBackStack(null)
                 .commit();
 
-
-
         handler = new Handler();
-
         RadarSettings.getInstance().init(getApplicationContext());
-
-
-
     }
+
     Intent windowService;
 
-    public  void initMainFragment()
-
-    {
-
+    public void initMainFragment() {
+        // Initialize your main fragment here
     }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-
-    }
-
 
     private void startRadarWindowService() {
         windowService = new Intent(this, RadarDrawView.class);
-
         startService(windowService);
-
-
     }
 
     private void requestStoragePermission() {
@@ -357,11 +231,11 @@ public class VPNCaptureActivity extends FragmentActivity {
     }
 
     private void closeVpn() {
-        VpnServiceHelper.changeVpnRunningStatus(this,false);
+        VpnServiceHelper.changeVpnRunningStatus(this, false);
     }
 
     private void startVPN() {
-        VpnServiceHelper.changeVpnRunningStatus(this,true);
+        VpnServiceHelper.changeVpnRunningStatus(this, true);
     }
 
     @Override
@@ -375,17 +249,20 @@ public class VPNCaptureActivity extends FragmentActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == REQUEST_OVERLAY_PERMISSION) {
-            if (Settings.canDrawOverlays(this)) {
-                startRadarWindowService();
-            } else {
+            if (!Settings.canDrawOverlays(this)) {
                 // Overlay permission not granted, handle accordingly
             }
         }
 
-
         if (requestCode == START_VPN_SERVICE_REQUEST_CODE && resultCode == RESULT_OK) {
             VpnServiceHelper.startVpnService(getApplicationContext());
-
         }
+    }
+
+    // Method to start the login activity
+    private void startLoginActivity() {
+        Intent intent = new Intent(VPNCaptureActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish(); // Finish the current activity
     }
 }
